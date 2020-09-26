d41a7787c92c5e7b66827322ab8bad8f3501afd7
Ryan Martinez <ryan.martinez@ohmvision.com>
2020-09-25 22:29:56 -0700

    init commit

---
+++ local-index.js
@@ -0,0 +1,28 @@
+/*jshint esversion: 6 */
+
+const dogs = [
+    {name: 'Roscoe'},
+    {name: 'Sammy'},
+    {name: 'Pookie'},
+    {name: 'Artichokes'},
+]
+
+main();
+
+function main(){
+    displayDogs();
+}
+
+function displayDogs(){
+    for(let dog of dogs){
+        addDogToUI(dog);
+    }
+}
+
+function addDogToUI(dog){
+    const dogUI = document.createElement('a');
+    dogUI.className = 'list-group-item';
+    dogUI.innerText = dog.name;
+
+    document.getElementById('dogs-list').appendChild(dogUI);
+}
